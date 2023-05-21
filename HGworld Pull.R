1c38b329a7b5e4e8f8b88a8fc73ce0fcc81fa7c0
bahamut19 <joshua.adams5@mail.com>
2023-05-20 23:46:22 -0400

    20230520
    Added HGworld Pull.R to pull complete translated text into a txt file. Sample Kitab-i-Badi.txt is included to show result of the script.

---
+++ HGworld Pull.R
@@ -0,0 +1,26 @@
+library(rvest)
+library(tidyverse)
+
+## Edit lines 6-8
+
+book <- "Kitab-i-Badi"
+pages <- 39
+column <- "GPT4_"
+
+urltable <- tibble(NPage = 1:pages, URL = paste0("https://www.hgworld.org/ctw/index.php?title=", book,"/Page", NPage))
+newbook <- tibble()
+
+urlpage <- 1:pages
+for (i in urlpage) {
+  url <- as.character(urltable[i,2])
+  webpage <- read_html(url)
+  htmltable <- html_table(webpage)
+  booktable <- tibble(Paragraph = (i*20-19):(i*20), Text = htmltable[[1]][[column]])
+  newbook <- bind_rows(newbook, booktable)
+}
+
+completebook <- paste0(newbook$Paragraph, " ", newbook$Text)
+finished <- gsub("\\s*\\([^\\)]+\\)", '', completebook)
+
+write_lines(finished, file = paste0(book, ".txt"))
+
